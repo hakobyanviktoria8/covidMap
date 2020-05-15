c7ae60b92b80cd3b79ebacd80c1feac4f2a3a928
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2020-05-16 03:21:40 +0400

    reactStrap, ifream

---
+++ src/CovidMap/Table.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Running from "./Running";
+
+class CountryTable extends React.Component{
+ render(){
+     return(
+         <div className="searchTable">
+            <table>
+                <thead>
+                <tr>
+                    <th style={{width:"45px"}}>N</th>
+                    <th style={{width:"280px"}}>Country</th>
+                    <th style={{width:"100px"}}>Confirmed</th>
+                    <th style={{width:"100px"}}>Recovered</th>
+                    <th style={{width:"100px"}}>Active</th>
+                    <th style={{width:"100px"}}>Deaths</th>
+                </tr>
+                </thead>
+                <Running />
+            </table>
+         </div>
+     )
+ }
+}
+export default CountryTable;
