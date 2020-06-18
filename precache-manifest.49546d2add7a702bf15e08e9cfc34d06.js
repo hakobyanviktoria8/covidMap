794eceb77ee9bd395c3912a0c19d670383184ab6
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2020-06-18 18:33:12 +0400

    Updates

---
+++ precache-manifest.49546d2add7a702bf15e08e9cfc34d06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c93e6989bc76d2835e08aebc201c75",
+    "url": "/covidMap/index.html"
+  },
+  {
+    "revision": "3329ff036052d43c2757",
+    "url": "/covidMap/static/css/2.71a46ad9.chunk.css"
+  },
+  {
+    "revision": "b5fed3b3d1e2f01f6223",
+    "url": "/covidMap/static/css/main.7718a2f1.chunk.css"
+  },
+  {
+    "revision": "3329ff036052d43c2757",
+    "url": "/covidMap/static/js/2.a5da4747.chunk.js"
+  },
+  {
+    "revision": "b5ec5e7a49ac4a51546296ea3abffcbc",
+    "url": "/covidMap/static/js/2.a5da4747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5fed3b3d1e2f01f6223",
+    "url": "/covidMap/static/js/main.5fdef74d.chunk.js"
+  },
+  {
+    "revision": "4c9d116a24e510ce4ac9",
+    "url": "/covidMap/static/js/runtime-main.d7f07dfa.js"
+  }
+]);
